ba1b461cfd8607a022435e3dc098d823f2022c26
bradleyc95 <bradleycox1995@gmail.com>
2023-04-18 18:58:22 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Bradley Cox on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
